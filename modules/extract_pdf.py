6d4c356c42f098d1d4664c464d7c59f1ac22cab7
myhloli <moe@myhloli.com>
2024-07-08 10:44:01 +0800

    fix：Solving the problem of unrendered content following extreme magnification on single pages.

--- modules/extract_pdf.py
+++ modules/extract_pdf.py
@@ -10,7 +10,13 @@ def load_pdf_fitz(pdf_path, dpi=72):
     for i in range(len(doc)):
         page = doc[i]
         pix = page.get_pixmap(matrix=fitz.Matrix(dpi/72, dpi/72))
-        image = Image.frombytes('RGB', [pix.width, pix.height], pix.samples)
+        image = Image.frombytes('RGB', (pix.width, pix.height), pix.samples)
+
+        # if width or height > 3000 pixels, don't enlarge the image
+        if pix.width > 3000 or pix.height > 3000:
+            pix = page.get_pixmap(matrix=fitz.Matrix(1, 1), alpha=False)
+            image = Image.frombytes('RGB', (pix.width, pix.height), pix.samples)
+
         # images.append(image)
         images.append(np.array(image)[:,:,::-1])
     return images
