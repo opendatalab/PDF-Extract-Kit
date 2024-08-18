b0a1d9384b9bdb3b0755aaf9bbed683613f690a2
zhangtianning <zhangtianning110@gmail.com>
2024-08-18 14:03:24 +0800

    add checklog

---
+++ check_log.sh
@@ -0,0 +1,30 @@
+
+for file in .log/*; 
+do 
+    ### skip if it is not a file
+    [ -f "$file" ] || continue
+    ## if head -n 1 file has string `is not` then delete this file
+    if [ "$(tail -n 3 "$file"|head -n 1|grep -c 'is not')" -eq 1 ]; then
+        echo "Deleting $file"
+        rm -f "$file"
+    fi
+done
+
+runningPID=`squeue -u zhangtianning.di | awk '{print $1}'`
+for log_file in .log/*; 
+do 
+    ### skip if it is not a file
+    [ -f "$log_file" ] || continue
+    ## get PID from log_file, the name rule is like $PID-ParseSciHUB.out
+    PID=$(echo $log_file|awk -F'/' '{print $2}'|awk -F'-' '{print $1}')
+    ## if the PID is not in runningPID, then delete this file
+    if [ "$(echo $runningPID|grep -c $PID)" -eq 0 ]; then
+        #echo "Deleting $log_file"
+        rm -f "$log_file"
+    else
+        line=$(tail -n 30 "$log_file"|grep Data|tail -n 1| sed 's/\x1B\[A//g'| tr -d '\r')
+        echo $log_file $line
+        #grep Error "$log_file" 
+    fi
+done
+#echo "$output"
